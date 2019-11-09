d2b6bc9210c63c29aee4e6bb06ae739e3f7865a6
yoming26 <57481702+yoming26@users.noreply.github.com>
2019-11-10 00:14:31 +0800

    created profile

--- resources/js/components/LandingPage.vue
+++ resources/js/components/LandingPage.vue
@@ -1,21 +1,23 @@
 <template>
     <div class="container-fluid">
-        <testresource/>
+        
     </div>
 </template>
 
  <script>
 
-import login from "./Login.vue"
+/* import login from "./Login.vue"
 import home from "./Home.vue"
 import register from "./Register.vue"
 import testresource from "./testresources.vue"
+import profile from "./profile.vue"
 export default {
     components: {
         login,
         home,
         register,
         testresource,
+        profile
     }
-}
+} */
 </script>
