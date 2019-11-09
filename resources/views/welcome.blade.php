f7f58f42663b2546b8e3e5f9055b2509bc5b4ae2
Allan Adan <allanadan1999@gmail.com>
2019-11-09 15:37:15 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,28 @@
         <title>Hackaton 2019</title>
 
         <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
+
+        <!-- from dan -->
+        link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="{{ asset('css/open-iconic-bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/animate.css')}}">
+        
+        <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/magnific-popup.css')}}">
+
+        <link rel="stylesheet" href="{{ asset('css/aos.css')}}">
+
+        <link rel="stylesheet" href="{{ asset('css/ionicons.min.css')}}">
+            
+            <link rel="stylesheet" href="{{ asset('css/bootstrap-datetimepicker.min.css')}}">
+            <link rel="stylesheet" href="{{ asset('css/nouislider.css')}}">
+
+        
+        <link rel="stylesheet" href="{{ asset('css/flaticon.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/icomoon.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/style.css')}}">
     </head>
     <body>
         
